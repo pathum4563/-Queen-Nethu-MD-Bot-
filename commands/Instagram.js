d88a4f7aca19e54b359e1b7e44239dd8c7932b61
Dark Shadow <127263909+shashikabot@users.noreply.github.com>
2023-05-29 01:01:18 +0530

    🍬🍧 𝐐𝓤ε𝔼𝐧 η𝕖Ⓣн𝕦 𝔪𝔡 🎃😈

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -3,10 +3,11 @@ cmd({
         pattern: "insta",
         desc: "download instagram post.",
         category: "downloader",
+        react: "💌",
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.reply('Need post url.')
+if(!text) return citel.reply('*පෝස්ට් url එක අවශ්‍යයි.*')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
