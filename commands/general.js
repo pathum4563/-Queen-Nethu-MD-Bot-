6d855d2b12b480ffe2690f0d1e3b59a81d74e511
Dark Shadow <127263909+shashikabot@users.noreply.github.com>
2023-05-30 02:51:05 +0530

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -132,3 +132,59 @@ cmd({
 
     }
 )
+//---------------------------------------------------------------------------
+cmd({
+      pattern: 'welcome$',
+    use: 'group',
+    fromMe: true,
+    desc: Lang.WELCOME_DESC
+}, (async (message, match) => {
+    var hg = await sql.getMessage(message.jid);
+    if (hg === false) {
+        await message.sendReply(Lang.NOT_SET_WELCOME);
+    } else {
+        await message.sendReply(Lang.WELCOME_ALREADY_SETTED + hg.message);
+    }
+}));
+//---------------------------------------------------------------------------
+cmd({
+    pattern: 'welcome (.*)',
+    fromMe: true,
+    use: 'group',
+    dontAddCommandList: true
+}, (async (message, match) => {
+    if (match[1] === 'delete') {
+        await message.send(Lang.WELCOME_DELETED);
+        return await sql.deleteMessage(message.jid, 'welcome');
+    }
+    await sql.setMessage(message.jid, 'welcome', match[1].replace(/&/g, '\n'));
+    return await message.sendReply(Lang.WELCOME_SETTED)
+}));
+//---------------------------------------------------------------------------
+cmd({
+    pattern: 'goodbye$',
+    fromMe: true,
+    desc: Lang.GOODBYE_DESC,
+    use: 'group'
+}, (async (message, match) => {
+    var hg = await sql.getMessage(message.jid, 'goodbye');
+    if (hg === false) {
+        await message.sendReply(Lang.NOT_SET_GOODBYE)
+    } else {
+        await message.send(Lang.GOODBYE_ALREADY_SETTED + hg.message);
+    }
+}));
+//---------------------------------------------------------------------------
+cmd({
+    pattern: 'goodbye (.*)',
+    fromMe: true,
+    dontAddCommandList: true,
+    use: 'group'
+}, (async (message, match) => {
+    if (match[1] === 'delete') {
+        await message.sendReply(Lang.GOODBYE_DELETED);
+        return await sql.deleteMessage(message.jid, 'goodbye');
+    }
+    await sql.setMessage(message.jid, 'goodbye', match[1].replace(/#/g, '\n'));
+    return await message.send(Lang.GOODBYE_SETTED)
+}));
