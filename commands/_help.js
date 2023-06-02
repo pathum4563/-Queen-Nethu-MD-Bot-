7499600b22e402bf291a21c62292c650c0d5e001
Dark Shadow <127263909+mrhansamala@users.noreply.github.com>
2023-06-02 12:42:28 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -1,11 +1,14 @@
 /**
+█▀█ █░█ █▀▀ █▀▀ █▄░█   █▄░█ █▀▀ ▀█▀ █░█ █░█   █▀▄▀█ █▀▄   █▄▄ █▀█ ▀█▀
+▀▀█ █▄█ ██▄ ██▄ █░▀█   █░▀█ ██▄ ░█░ █▀█ █▄█   █░▀░█ █▄▀   █▄█ █▄█ ░█░
+
  Copyright (C) 2023.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Queen-Nethu-Md
- * @author : MR.Hansamala <https://github.com/shashikabot>
- * @description : Queen-Nethu,A Multi-functional whatsapp bot.
+ * @project_name : QueenNethu-Md
+ * @author : MR.Hansamala <https://github.com/mrhansamala>
+ * @description : Nethu,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
